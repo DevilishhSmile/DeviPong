d587a8afd9d0de5e0c9c669e3f5d87fea60d4fee
CamiloGato <cm.andres022@hotmail.com>
2025-03-23 22:48:05 -0500

    Update: Added game assets and base

---
+++ Assets/Settings/Input/InputReader.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static InputSystem_Actions;
+
+
+public interface IInputReader
+{
+    Vector2 Direction { get; }
+    void EnablePlayerAction();
+}
+    
+public class InputReader : ScriptableObject, IPlayerActions, IInputReader
+{
+    public event UnityAction<Vector2> Move = delegate { };
+
+    private InputSystem_Actions _inputActions;
+
+    public Vector2 Direction => _inputActions.Player.Move.ReadValue<Vector2>();
+    
+    #if UNITY_EDITOR
+    private const string AssetPath = "Assets/Settings/Input/InputReader.asset"; 
+    
+    [InitializeOnLoadMethod]
+    static void RunOnStart()
+    {
+        InputReader inputReader = AssetDatabase.LoadAssetAtPath<InputReader>(AssetPath);
+        if (inputReader == null)
+        {
+            inputReader = CreateInstance<InputReader>();
+            AssetDatabase.CreateAsset(inputReader, AssetPath);
+            AssetDatabase.SaveAssets();
+        }
+        AssetDatabase.Refresh();
+    }
+    #endif
+    public void EnablePlayerAction()
+    {
+        if (_inputActions == null)
+        {
+            _inputActions = new InputSystem_Actions();
+            _inputActions.Player.SetCallbacks(this);
+        }
+        _inputActions.Player.Enable();
+    }
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Move.Invoke(context.ReadValue<Vector2>());
+    }
+}
---
+++ Assets/Systems/Paddle/PaddleMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Systems.Paddle
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PaddleMovement : MonoBehaviour
+    {
+        [SerializeField] private float speed = 10f;
+        [SerializeField] private InputReader inputReader;
+        
+        private Rigidbody2D _rigidbody2D;
+        private Transform _transform;
+        private float _verticalInput;
+        
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _transform = transform;
+        }
+
+        private void Start()
+        {
+            inputReader.Move += ChangeDirection;
+            inputReader.EnablePlayerAction();
+        }
+
+        private void ChangeDirection(Vector2 direction)
+        {
+            _verticalInput = direction.y;
+        }
+
+        private void FixedUpdate()
+        {
+            _rigidbody2D.linearVelocityY = _verticalInput * speed;
+        }
+    }
+}
